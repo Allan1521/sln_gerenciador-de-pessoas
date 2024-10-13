55ccab0b1a57c3a693da73b742bdaa7d0018baa7
Allan <>
2024-10-13 19:25:46 -0300

    Gerenciador de Pessoa faltando gerar doc

--- PrjGerenciadorPessoas/Form1.Designer.cs
+++ PrjGerenciadorPessoas/Form1.Designer.cs
@@ -28,28 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lstPessoas = new ListBox();
+            btnGerarDoc = new Button();
+            btnExcluirLista = new Button();
+            pnlForm = new Panel();
+            btnLimpar = new Button();
+            btnExcluir = new Button();
+            btnSalvar = new Button();
+            lblNome = new Label();
+            txtIdade = new TextBox();
+            txtNome = new TextBox();
+            btnCriarPessoa = new Button();
+            btnVerPessoa = new Button();
             lblIdade = new Label();
             lblMensagem = new Label();
-            btnVerPessoa = new Button();
-            btnCriarPessoa = new Button();
-            txtNome = new TextBox();
-            txtIdade = new TextBox();
-            lblNome = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            listBox1 = new ListBox();
+            pnlForm.SuspendLayout();
             SuspendLayout();
+            // 
+            // lstPessoas
+            // 
+            lstPessoas.FormattingEnabled = true;
+            lstPessoas.ItemHeight = 15;
+            lstPessoas.Location = new Point(549, 50);
+            lstPessoas.Name = "lstPessoas";
+            lstPessoas.Size = new Size(362, 259);
+            lstPessoas.TabIndex = 16;
+            lstPessoas.SelectedIndexChanged += lstPessoas_SelectedIndexChanged;
+            // 
+            // btnGerarDoc
+            // 
+            btnGerarDoc.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnGerarDoc.ForeColor = SystemColors.ActiveCaptionText;
+            btnGerarDoc.Location = new Point(549, 315);
+            btnGerarDoc.Name = "btnGerarDoc";
+            btnGerarDoc.Size = new Size(196, 37);
+            btnGerarDoc.TabIndex = 20;
+            btnGerarDoc.Text = "&Gerar Documento";
+            btnGerarDoc.UseVisualStyleBackColor = true;
+            btnGerarDoc.Click += btnGerarDoc_Click;
+            // 
+            // btnExcluirLista
+            // 
+            btnExcluirLista.BackColor = Color.Red;
+            btnExcluirLista.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnExcluirLista.ForeColor = SystemColors.Window;
+            btnExcluirLista.Location = new Point(787, 315);
+            btnExcluirLista.Name = "btnExcluirLista";
+            btnExcluirLista.Size = new Size(124, 44);
+            btnExcluirLista.TabIndex = 21;
+            btnExcluirLista.Text = "E&xcluir Lista";
+            btnExcluirLista.UseVisualStyleBackColor = false;
+            btnExcluirLista.Click += btnExcluirLista_Click;
+            // 
+            // pnlForm
+            // 
+            pnlForm.BorderStyle = BorderStyle.FixedSingle;
+            pnlForm.Controls.Add(btnLimpar);
+            pnlForm.Controls.Add(btnExcluir);
+            pnlForm.Controls.Add(btnSalvar);
+            pnlForm.Controls.Add(lblNome);
+            pnlForm.Controls.Add(txtIdade);
+            pnlForm.Controls.Add(txtNome);
+            pnlForm.Controls.Add(btnCriarPessoa);
+            pnlForm.Controls.Add(btnVerPessoa);
+            pnlForm.Controls.Add(lblIdade);
+            pnlForm.Location = new Point(23, 50);
+            pnlForm.Name = "pnlForm";
+            pnlForm.Size = new Size(506, 302);
+            pnlForm.TabIndex = 22;
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.BackColor = SystemColors.Window;
+            btnLimpar.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLimpar.ForeColor = SystemColors.ActiveCaptionText;
+            btnLimpar.Location = new Point(210, 256);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.Size = new Size(285, 44);
+            btnLimpar.TabIndex = 28;
+            btnLimpar.Text = "&Limpar";
+            btnLimpar.UseVisualStyleBackColor = false;
+            btnLimpar.Click += btnLimpar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.BackColor = Color.Red;
+            btnExcluir.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnExcluir.ForeColor = SystemColors.Window;
+            btnExcluir.Location = new Point(51, 256);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(124, 44);
+            btnExcluir.TabIndex = 27;
+            btnExcluir.Text = "&Excluir";
+            btnExcluir.UseVisualStyleBackColor = false;
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSalvar.ForeColor = SystemColors.ActiveCaptionText;
+            btnSalvar.Location = new Point(371, 186);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(124, 44);
+            btnSalvar.TabIndex = 26;
+            btnSalvar.Text = "&Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            lblNome.ForeColor = SystemColors.HotTrack;
+            lblNome.Location = new Point(12, 42);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(74, 28);
+            lblNome.TabIndex = 25;
+            lblNome.Text = "Nome:";
+            // 
+            // txtIdade
+            // 
+            txtIdade.BackColor = SystemColors.Info;
+            txtIdade.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            txtIdade.Location = new Point(111, 101);
+            txtIdade.Name = "txtIdade";
+            txtIdade.Size = new Size(100, 34);
+            txtIdade.TabIndex = 24;
+            // 
+            // txtNome
+            // 
+            txtNome.BackColor = SystemColors.Info;
+            txtNome.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            txtNome.Location = new Point(111, 39);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(371, 34);
+            txtNome.TabIndex = 23;
+            // 
+            // btnCriarPessoa
+            // 
+            btnCriarPessoa.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCriarPessoa.Location = new Point(51, 186);
+            btnCriarPessoa.Name = "btnCriarPessoa";
+            btnCriarPessoa.Size = new Size(124, 44);
+            btnCriarPessoa.TabIndex = 22;
+            btnCriarPessoa.Text = "&Criar Pessoa";
+            btnCriarPessoa.UseVisualStyleBackColor = true;
+            btnCriarPessoa.Click += btnCriarPessoa_Click;
+            // 
+            // btnVerPessoa
+            // 
+            btnVerPessoa.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnVerPessoa.ForeColor = SystemColors.ActiveCaptionText;
+            btnVerPessoa.Location = new Point(210, 186);
+            btnVerPessoa.Name = "btnVerPessoa";
+            btnVerPessoa.Size = new Size(124, 44);
+            btnVerPessoa.TabIndex = 21;
+            btnVerPessoa.Text = "&Ver Pessoa";
+            btnVerPessoa.UseVisualStyleBackColor = true;
+            btnVerPessoa.Click += btnVerPessoa_Click;
             // 
             // lblIdade
             // 
             lblIdade.AutoSize = true;
             lblIdade.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
             lblIdade.ForeColor = SystemColors.HotTrack;
-            lblIdade.Location = new Point(28, 152);
+            lblIdade.Location = new Point(17, 107);
             lblIdade.Name = "lblIdade";
             lblIdade.Size = new Size(69, 28);
-            lblIdade.TabIndex = 1;
+            lblIdade.TabIndex = 20;
             lblIdade.Text = "Idade:";
             // 
             // lblMensagem
@@ -62,134 +208,40 @@
             lblMensagem.Size = new Size(0, 28);
             lblMensagem.TabIndex = 7;
             // 
-            // btnVerPessoa
-            // 
-            btnVerPessoa.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            btnVerPessoa.ForeColor = SystemColors.ActiveCaptionText;
-            btnVerPessoa.Location = new Point(221, 231);
-            btnVerPessoa.Name = "btnVerPessoa";
-            btnVerPessoa.Size = new Size(124, 44);
-            btnVerPessoa.TabIndex = 8;
-            btnVerPessoa.Text = "&Ver Pessoa";
-            btnVerPessoa.UseVisualStyleBackColor = true;
-            btnVerPessoa.Click += btnVerPessoa_Click;
-            // 
-            // btnCriarPessoa
-            // 
-            btnCriarPessoa.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            btnCriarPessoa.Location = new Point(62, 231);
-            btnCriarPessoa.Name = "btnCriarPessoa";
-            btnCriarPessoa.Size = new Size(124, 44);
-            btnCriarPessoa.TabIndex = 9;
-            btnCriarPessoa.Text = "&Criar Pessoa";
-            btnCriarPessoa.UseVisualStyleBackColor = true;
-            btnCriarPessoa.Click += btnCriarPessoa_Click;
-            // 
-            // txtNome
-            // 
-            txtNome.BackColor = SystemColors.Info;
-            txtNome.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            txtNome.Location = new Point(122, 84);
-            txtNome.Name = "txtNome";
-            txtNome.Size = new Size(371, 34);
-            txtNome.TabIndex = 10;
-            txtNome.TextChanged += txtNome_TextChanged;
-            // 
-            // txtIdade
-            // 
-            txtIdade.BackColor = SystemColors.Info;
-            txtIdade.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            txtIdade.Location = new Point(122, 146);
-            txtIdade.Name = "txtIdade";
-            txtIdade.Size = new Size(100, 34);
-            txtIdade.TabIndex = 11;
-            txtIdade.TextChanged += txtIdade_TextChanged;
-            // 
-            // lblNome
-            // 
-            lblNome.AutoSize = true;
-            lblNome.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
-            lblNome.ForeColor = SystemColors.HotTrack;
-            lblNome.Location = new Point(23, 87);
-            lblNome.Name = "lblNome";
-            lblNome.Size = new Size(74, 28);
-            lblNome.TabIndex = 12;
-            lblNome.Text = "Nome:";
-            lblNome.Click += lblNome_Click;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(377, 231);
-            button1.Name = "button1";
-            button1.Size = new Size(116, 44);
-            button1.TabIndex = 13;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(62, 295);
-            button2.Name = "button2";
-            button2.Size = new Size(120, 39);
-            button2.TabIndex = 14;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(221, 296);
-            button3.Name = "button3";
-            button3.Size = new Size(272, 38);
-            button3.TabIndex = 15;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(558, 84);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(362, 244);
-            listBox1.TabIndex = 16;
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(932, 398);
-            Controls.Add(listBox1);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(lblNome);
-            Controls.Add(txtIdade);
-            Controls.Add(txtNome);
-            Controls.Add(btnCriarPessoa);
-            Controls.Add(btnVerPessoa);
+            ClientSize = new Size(932, 487);
+            Controls.Add(pnlForm);
+            Controls.Add(btnExcluirLista);
+            Controls.Add(btnGerarDoc);
+            Controls.Add(lstPessoas);
             Controls.Add(lblMensagem);
-            Controls.Add(lblIdade);
             ForeColor = SystemColors.ActiveCaptionText;
             Name = "Form1";
             Text = "Gerenciador de Pessoas";
             Load += Form1_Load;
+            pnlForm.ResumeLayout(false);
+            pnlForm.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+        private ListBox lstPessoas;
+        private Button btnGerarDoc;
+        private Button btnExcluirLista;
+        private Panel pnlForm;
+        private Button btnLimpar;
+        private Button btnExcluir;
+        private Button btnSalvar;
+        private Label lblNome;
+        private TextBox txtIdade;
+        private TextBox txtNome;
+        private Button btnCriarPessoa;
+        private Button btnVerPessoa;
         private Label lblIdade;
         private Label lblMensagem;
-        private Button btnVerPessoa;
-        private Button btnCriarPessoa;
-        private TextBox txtNome;
-        private TextBox txtIdade;
-        private Label lblNome;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private ListBox listBox1;
     }
 }
--- PrjGerenciadorPessoas/Form1.cs
+++ PrjGerenciadorPessoas/Form1.cs
@@ -1,10 +1,9 @@
-using SamAlvarenga.PrjHelloWorld.Models;
+using Allan1521.PrjHelloWord.Models;
 
 namespace PrjGerenciadorPessoas
-
 {
     public partial class Form1 : Form
-    {
+    {   
         //declaramos class
         private Pessoa pessoa;
         private List<Pessoa> pessoas = new List<Pessoa>();  //Criando o objeto lista de pessoas 
@@ -16,31 +15,13 @@ namespace PrjGerenciadorPessoas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Pessoa pessoinha = new Pessoa();
+            lstPessoas.DisplayMember = "Nome";
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblNome_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblIdade_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void btnVerPessoa_Click(object sender, EventArgs e)
         {
+            MessageBox.Show($"Nome: {pessoa.Nome}\n Idade: {pessoa.getIdadeFormatada()}", "Dados Pessoais", MessageBoxButtons.OK, MessageBoxIcon.Information);
             //lblMensagem.Text = $"{pessoa.Nome} - {pessoa.getIdadeFormatada()}";
             //for (int contador = 0; contador < this.pessoa.Count; contador++)
             //{
@@ -48,26 +29,15 @@ namespace PrjGerenciadorPessoas
             //}
 
 
-            foreach (var item in pessoas)
-            {
-                lblMensagem.Text = $"{lblMensagem.Text}\n {pessoa.Nome}";
-            }
+            //foreach (var item in pessoas)
+            //{
+            //    lblMensagem.Text = $"{lblMensagem.Text}\n {pessoa.Nome}";
+            //}
         }
-
+                    //BOTÃO LIMPAR CAIXAS DADOS DA PESSOAS
         private void btnLimpar_Click(object sender, EventArgs e)
         {
-            DialogResult apagarLista = MessageBox.Show("Tem certeza que deseja limpar a lista de pessoas?",
-                "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
-            if (apagarLista == DialogResult.Yes)
-            {
-                pessoas.Clear();
-                lstPessoas.Items.Clear();
-
-                txtIdade.Clear();
-                txtNome.Clear();
-                txtNome.Focus();
-
-            }
+            ResetForm();
         }
 
         private void btnCriarPessoa_Click(object sender, EventArgs e)
@@ -85,70 +55,132 @@ namespace PrjGerenciadorPessoas
             }
             catch (FormatException)
             {
-                lblMensagem.Text = "Apenas números podem ser utilizados no campo idade!";
+                MessageBox.Show("Apenas números podem ser utilizados no campo idade!", "Campo Inválido", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (Exception ex)
             {
-                lblMensagem.Text = ex.Message;
+                MessageBox.Show(ex.Message);
+                return;
 
             }
-
             txtNome.Clear();
             txtIdade.Clear();
             txtIdade.Focus();
-
-            MessageBox.Show("Pessoa adicionada com sucesso!", "info",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
-            lblMensagem.Text = "Pessoa adicionada com sucesso!";
         }
 
         private void btnExcluir_Click(object sender, EventArgs e)
         {
 
-            if (lstPessoas.SelectedItems == null)
+            //if (lstPessoas.SelectedItems == null)
+            //{
+
+            //    MessageBox.Show("Não há itens selecionar na lista",
+            //        "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //    lblMensagem.Text = "Não há itens selecionados na lista";
+
+            //}
+            //else
+            //{
+            //    lstPessoas.Item.Remove(lslPessoas.Selected);
+            //}
+
+            DialogResult apagarDaLista = MessageBox.Show($"Tem certeza que deseja excluir {lstPessoas.SelectedItem.ToString()} da lista?", "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+
+            if (apagarDaLista == DialogResult.Yes)
             {
-
-                MessageBox.Show("Não há itens selecionar na lista",
-                    "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                lblMensagem.Text = "Não há itens selecionados na lista";
-
+                lstPessoas.Items.Remove(lstPessoas.SelectedItem); //Remover pessoa da lisBox
+                lstPessoas.SelectedItem = null; //remover da lista de pessoas 
+                ResetForm();
             }
-            else
+        }
+                    //BOTÃO DE ATUALIZAR E SALVAR PESSOA
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (lstPessoas.SelectedItem != null) //verificar se há um item na lista 
             {
-                lstPessoas.Item.Remove(lslPessoas.Selected);
+                this.pessoa = (Pessoa)lstPessoas.SelectedItem; //casting
+                this.pessoa.Nome = txtNome.Text;
+                this.pessoa.Idade = Convert.ToInt32(txtIdade.Text);
+                lstPessoas.Items[lstPessoas.SelectedIndex] = this.pessoa;
+                MessageBox.Show(" Dados atualizados com sucesso!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetForm();
             }
+        }
 
+        private void lstPessoas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnSalvar.Enabled = true;
+            btnExcluir.Enabled = true;
 
-            private void txtNome_TextChanged(object sender, EventArgs e)
+            if (lstPessoas.SelectedItem != null)
             {
-
+                this.pessoa = (Pessoa)lstPessoas.SelectedItem; //Casting
+                txtNome.Text = this.pessoa.Nome;
+                txtIdade.Text = this.pessoa.Idade.ToString();
             }
+        }
 
-            private void txtIdade_TextChanged(object sender, EventArgs e)
+        private void btnExcluirLista_Click(object sender, EventArgs e)
+        {
+            DialogResult ApagarLista = MessageBox.Show("Tem certeza que deseja limpar a lista de pessoas?", "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+
+            if (ApagarLista == DialogResult.Yes)
+            {
+                lstPessoas.Items.Clear();
+
+                ResetForm();
+            }
+        }
+        private void ResetForm()
+        {
+            txtIdade.Clear();
+            txtNome.Clear();
+            txtNome.Focus();
+
+            lstPessoas.SelectedItem = null; //desmarcando a seleção 
+            btnSalvar.Enabled = false;      //deixar inativo o botão
+            btnVerPessoa.Enabled = false;   //deixar inativo o botão
+            btnExcluir.Enabled = false;     //deixar inativo o botão
+        }
+
+        private void btnGerarDoc_Click(object sender, EventArgs e)
+        {
+            string conteudoArquivo = $"Nome: {this.pessoa.Nome} - Idade: {this.pessoa.getIdadeFormatada()}";
+            try //É onde você coloca o código que pode gerar uma exceção.
+                //Se tudo correr bem, o código dentro do bloco try
+                //será executado normalmente e o bloco catch será ignorado.
             {
 
+                File.WriteAllText("relatorio/relatorio.txt", conteudoArquivo);
+
+                MessageBox.Show("Relatório gerado com sucesso", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ResetForm();
+            }
+            catch (DirectoryNotFoundException ex) // É onde você coloca o código
+                                                  // que será executado caso ocorra uma exceção no
+                                                  // bloco try. O tipo de exceção que cada bloco catch captura é
+                                                  // especificado entre parênteses.
+            {
+
+                MessageBox.Show("Houve um erro na criação do relatório. Pasta não encontrada!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
 
         }
-
-        private void txtNome_TextChanged(object sender, EventArgs e)
+        private void gerarRelatorio(string conteudo)
         {
+            File.WriteAllText("relatorio/relatorio.txt", conteudo);
 
-        }
 
-        private void txtIdade_TextChanged(object sender, EventArgs e)
-        {
+            MessageBox.Show("Relatório gerado com sucesso!", "Info",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
+            ResetForm();
         }
     }
+}
